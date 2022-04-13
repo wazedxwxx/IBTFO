30f73d69e90fbc7d8d479b4e03d405b5ae6b0b81
XINXIN WANG <59836980+wazedxwxx@users.noreply.github.com>
2022-04-13 19:15:56 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ extensions = [ 'sphinx.ext.mathjax']
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
-project = u'PeleC'
-copyright = u'AMReX Copyright (c) 2017, The Regents of the University of California, through Lawrence Berkeley National Laboratory and the Alliance for Sustainable Energy, LLC., through National Renewable Energy Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All rights reserved.'
-author = u'J.B. Bell, M.S. Day, E. Motheau, D. Graves, M. Henry de Frahan, R.W. Grout, J. Rood'
+project = u'IBTFO'
+copyright = u'IBTFO Copyright (c) 2022, The Regents of the National University of Defense technology. All rights reserved.'
+author = u'Xinxin Wang, Jianhan Liang'
 version = u'0.1'
 release = u'0.1'
 language = None
@@ -17,14 +17,14 @@ numfig = True
 numfig_format = {'figure': '%s', 'table': '%s', 'code-block': '%s'}
 html_theme = 'sphinx_rtd_theme'
 #html_static_path = ['_static']
-htmlhelp_basename = 'PeleCdoc'
+htmlhelp_basename = 'IBTFOdoc'
 latex_elements = { }
 latex_documents = [
-    (master_doc, 'PeleC.tex', u'PeleC Documentation',
+    (master_doc, 'IBTFO.tex', u'IBTFO Documentation',
      author, 'manual'),
 ]
 texinfo_documents = [
-    (master_doc, 'PeleC', u'PeleC Documentation',
-     author, 'PeleC', 'One line description of project.',
+    (master_doc, 'IBTFO', u'IBTFO Documentation',
+     author, 'IBTFO', 'One line description of project.',
      'Miscellaneous'),
 ]
